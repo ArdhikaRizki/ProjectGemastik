26c13d733dab68bce413f3a7a07788ab43906a74
rizkiii161 <ardhikaz30@gmail.com>
2025-07-19 17:35:05 +0700

    add routes

---
+++ lib/Routes/route_names.dart
@@ -0,0 +1,5 @@
+class RouteNames {
+  static String home = '/home';
+  static String register = '/register';
+  static String login = '/login';
+}
---
+++ lib/Routes/route_pages.dart
@@ -0,0 +1,9 @@
+import 'package:get/get_navigation/get_navigation.dart';
+import 'package:project_gemastik/LoginRegister/View/RegisterView.dart';
+import 'package:project_gemastik/Routes/route_names.dart';
+
+class RoutesPage {
+  static List<GetPage> page = [
+    GetPage(name: RouteNames.register , page: () => Registerview())
+  ];
+}
