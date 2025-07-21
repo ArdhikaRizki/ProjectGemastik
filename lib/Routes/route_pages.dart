fdf97367051674151cda826688b96bedaabba23d
rizkiii161 <ardhikaz30@gmail.com>
2025-07-21 19:55:06 +0700

    change productPafe to Dashboard Folder

--- lib/Routes/route_pages.dart
+++ lib/Routes/route_pages.dart
@@ -1,6 +1,6 @@
 import 'package:get/get_navigation/get_navigation.dart';
 import 'package:project_gemastik/LoginRegister/View/RegisterView.dart';
-import 'package:project_gemastik/ProductPage/View/ProductPageView.dart';
+import 'package:project_gemastik/Dashboard/View/ProductPageView.dart';
 import 'package:project_gemastik/Routes/route_names.dart';
 import 'package:project_gemastik/profile/view/profile_view.dart';
 
